b3616ec826c77cff07aedcfcab7de8cbdf451b16
Mukul Sharma <mukul.sharma@gdn-commerce.com>
2022-09-04 16:04:33 +0530

    Initial Commit

---
+++ src/components/about/About.js
@@ -0,0 +1,54 @@
+import React from 'react';
+import Style from './About.module.scss';
+import Terminal from "./Terminal";
+import {Box} from "@mui/material";
+import {info} from "../../info/Info";
+
+
+export default function About() {
+    const firstName = info.firstName.toLowerCase()
+
+    function aboutMeText() {
+        return <>
+            <p>
+                <span style={{color: info.baseColor}}>About Me</span>
+            </p>
+            <p>
+                {info.bio}
+            </p>
+        </>;
+    }
+
+    function skillsText() {
+        return <>
+            <p><span style={{color: info.baseColor}}>Skills </span></p>
+            <p style={{color: info.baseColor}}> Proficient With</p>
+            <ul className={Style.skills}>
+                {info.skills.proficientWith.map((proficiency, index) => <li key={index}>{proficiency}</li>)}
+            </ul>
+            <p style={{color: info.baseColor}}> Exposed To</p>
+            <ul className={Style.skills}>
+                {info.skills.exposedTo.map((skill, index) => <li key={index}>{skill}</li>)}
+            </ul>
+        </>;
+    }
+
+    function miscText() {
+        return <>
+            <p><span style={{color: info.baseColor}}>hobbies/interests </span></p>
+            <ul>
+                {info.hobbies.map((hobby, index) => (
+                    <li key={index}><Box component={'span'} mr={'1rem'}>{hobby.emoji}</Box>{hobby.label}</li>
+                ))}
+            </ul>
+        </>;
+    }
+
+    return (
+        <Box display={'flex'} flexDirection={'column'} alignItems={'center'} mt={'3rem'}>
+            <Terminal text={aboutMeText()}/>
+            <Terminal text={skillsText()}/>
+            <Terminal text={miscText()}/>
+        </Box>
+    )
+}
---
+++ src/info/Info.js
@@ -0,0 +1,111 @@
+import self from "../img/self.png"
+import mock1 from "../img/mock1.png"
+import mock2 from "../img/mock2.png"
+import mock3 from "../img/mock3.png"
+import mock4 from "../img/mock4.png"
+
+/* Hi there! Thanks for checking out my portfolio template. Be sure to read the comments to get a better understanding of
+how to make this template work best for you! */
+
+export let colors = ["rgb(0,255,164)", "rgb(166,104,255)"];
+/*
+I highly recommend using a gradient generator like https://gradientgenerator.paytonpierce.dev/ to generate a pair of colors that you like.
+These colors will be used to style your name on the homepage, the background of your picture, and some other accents throughout
+the site.
+ */
+
+
+/*
+So let's get started! Some of the info below is pretty self-explanatory, like 'firstName' and 'bio'. I'll try to explain anything
+that might not be obvious right off the bat :) I recommend looking at the template example live using "npm start" to get an idea
+of what each of the values mean.
+ */
+
+export const info = {
+    firstName: "Mahima",
+    lastName: "Sharma",
+    initials: "ms", // the example uses first and last, but feel free to use three or more if you like.
+    position: "a Digital Marketing expert",
+    selfPortrait: self, // don't change this unless you want to name your self-portrait in the "img" folder something else!
+    gradient: `-webkit-linear-gradient(135deg, ${colors})`, // don't change this either
+    baseColor: colors[0],
+    miniBio: [ // these are just some "tidbits" about yourself. You can look at mine https://paytonjewell.github.io/#/ for an example if you'd like
+        {
+            emoji: '☕',
+            text: 'fueled by coffee'
+        },
+        {
+            emoji: '🌎',
+            text: 'based in the Ireland'
+        },
+        {
+            emoji: "💼",
+            text: "Freelancer"
+        },
+        {
+            emoji: "📧",
+            text: "mahi.sharma1123@gmail.com"
+        }
+    ],
+    socials: [
+        {
+            link: "https://www.facebook.com/mahi.sharma.7 ",
+            icon: 'fa fa-facebook',
+            label: 'facebook'
+        },
+        {
+            link: "https://www.instagram.com/mahima_sharma10/?igshid=YmMyMTA2M2Y%3D",
+            icon: 'fa fa-instagram',
+            label: 'instagram'
+        },
+        {
+            link: "https://www.linkedin.com/in/mahima-creativewriter/",
+            icon: "fa fa-linkedin",
+            label: 'linkedin'
+        }
+// Feel free to remove any of these that you don't have. I'll write an FAQ on how to add new ones later, let me know if you have a request for an icon!
+// Just change the links so that they lead to your social profiles.
+
+    ],
+    bio: "I am a highly motivated and versatile graduate of Mass Communication with excellent interpersonal skills, having more than one year of experience with a digital marketing company, where my responsibilities are client services and writing content for social media platforms such as Facebook, Instagram, LinkedIn, and Youtube. I am a focused and goal-oriented individual who can manage multiple tasks simultaneously. Excellent communication skills honed by comfortable interactions with clients from the telecom industries, media industries, and social media agencies. Recently I completed a Master of Science in Digital Marketing from IT Carlow, As part of my education I worked on real-world projects to improve the digital marketing strategies for firms such as FoodCloud, SelectAccess, WIT Arena, and CookingWithSapana based out of Ireland.",
+    skills:
+        {
+            proficientWith: ['Social Media Marketing', 'Content Creation', 'Photo Content', 'Video Content', 'Content Marketing', 'Daily Social Media Management Service', 'Google Ads', 'Email Marketing'],
+            exposedTo: ['Canva', 'Photoshop']
+        }
+    ,
+    hobbies: [
+        {
+            label: 'movies',
+            emoji: '🎥'
+        },
+        {
+            label: 'cooking',
+            emoji: '🌶'
+        }
+// Same as above, change the emojis to match / relate to your hobbies or interests.
+// You can also remove the emojis if you'd like, I just think they look cute :P
+    ],
+    portfolio: [ // This is where your portfolio projects will be detailed
+        {
+            title: "Eurasia Supermarket",
+            live: "https://www.instagram.com/eurasiasupermarket/?igshid=YmMyMTA2M2Y%3D", //this should be a link to the live version of your project, think github pages, netlify, heroku, etc. Or your own domain, if you have it.
+            image: mock1
+        },
+        {
+            title: "Sheela Palace",
+            live: "https://www.instagram.com/sheelapalacerestaurant/?igshid=YmMyMTA2M2Y%3D",
+            image: mock2
+        },
+        {
+            title: "Locus Occupational Therapy app",
+            live: "https://www.instagram.com/locus_fct/",
+            image: mock3
+        },
+        {
+            title: "Spice Seeker",
+            live: "https://www.instagram.com/spiceseekerhere/?igshid=YmMyMTA2M2Y%3D",
+            image: mock4
+        },
+    ]
+}
